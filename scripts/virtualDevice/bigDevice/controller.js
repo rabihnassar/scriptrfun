721e6293c6ce2e8b6c3856f702e1036c5066a5fa
Rabih Nassar <rabih.nassar@apstrata.com>
2016-06-06 11:17:07 -0400

    bug fixes

--- scripts/virtualDevice/bigDevice/controller.js
+++ scripts/virtualDevice/bigDevice/controller.js
@@ -10,7 +10,13 @@ function process(msg) {
     client.send("display1", JSON.stringify(msg))
     client.send("btn", msg.value)
 
-    if (msg.id=="slider") client.send("gauge", msg.value)
+    if (msg.id=="slider") {
+        client.send("signal2", "on")
+        client.send("signal3", "on")
+        client.send("signal2", "off")
+        client.send("signal3", "off")
+		client.send("gauge", msg.value)
+    }
 
     if (msg.id=="push1") client.send("signal1", "on")
     if (msg.id=="push2") client.send("signal2", "on")
@@ -23,7 +29,7 @@ function process(msg) {
     if (msg.id=="send") {
         client.send("display", "Sending values now")
 
-        for (var i=0; i<1000; i=i+10) {
+        for (var i=0; i<2000; i=i+10) {
           client.send("gauge", i)
           client.send("signal2", "on")
           client.send("signal2", "off")
